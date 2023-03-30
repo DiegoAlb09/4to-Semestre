186c451ecef1489f993ec6d60addba380bb6344d
DiegoAlb09 <diegoalb04hat@gmail.com>
2023-03-30 11:49:56 -0600

    Inicios en Js

---
+++ Curso_Javascript_React/Template_String.js
@@ -0,0 +1,14 @@
+//Temlate String
+
+const nombre = 'Juan'
+const apellido = 'Rosales'
+
+const nombreCompleto = `${nombre} ${apellido}`
+
+console.log(nombreCompleto)
+
+function getSaludo(nombre){
+    return 'Hola ' + nombre
+}
+
+console.log(`Este es un texto: ${getSaludo(nombre)}`)
---
+++ Curso_Javascript_React/Variables_Constantes.js
@@ -0,0 +1,21 @@
+//Variables y Constantes
+
+//El valor no se puede cambiar
+
+const nombre = 'Juan'
+const apellido = 'Rosales'
+
+//El valor puede cambiar
+
+let valorDado = 5
+valorDado = 4
+
+console.log(nombre, apellido, valorDado)
+
+if(true){
+    const nombre = 'Manuel'
+    console.log(nombre)
+
+    let valorDado = 6
+    console.log(valorDado)
+}
